254a938e7f9b83846f6e5a57083cce437ae8672d
hobeomcheon <cjs414ghqja@gmail.com>
2021-11-18 20:10:23 +0900

    ref, reactive 사용구분

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div class="name">
     {{ name }}
   </div>
+  <div>
+    {{ name2 }}
+  </div>
   <button
       class="btn btn-primary"
       v-on:click="updateName"
@@ -11,18 +14,27 @@
 </template>
 
 <script>
+// 템플릿에서도 동시에 값이 변경되기위해 추가해야함
+import { ref } from 'vue';       // ref : 기본자료형
+import { reactive } from 'vue';  // reactive : Object, Array
 
 export default {
   setup() {
-    let name = 'hobeom coder';
+    const name = ref('hobeom coder');
+    const name2 = reactive({
+      id : 1
+    });
 
     const updateName = () => {
-      name = 'change coder';
+      //
+      name.value = 'change coder';   // ref를 사용할 때는 name.value 형태로 사용
+      name2.id = 2;                  // 안에 들어있는 값을 바로 접근 가능
       console.log(name);
     };
 
     return {
       name,
+      name2,
       updateName,
     };
   }
