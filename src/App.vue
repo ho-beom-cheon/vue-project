a3b6c3cbb8b07342fd599b7856f0c7978fc0be81
hobeomcheon <cjs414ghqja@gmail.com>
2021-11-18 19:58:51 +0900

    버튼 이벤트로 간단한 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="name">
+    {{ name }}
+  </div>
+  <button
+      class="btn btn-primary"
+      v-on:click="updateName"
+  >
+    Click
+  </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    let name = 'hobeom coder';
+
+    const updateName = () => {
+      name = 'change coder';
+      console.log(name);
+    };
+
+    return {
+      name,
+      updateName,
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.name {
+  color: red;
 }
 </style>
