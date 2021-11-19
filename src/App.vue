02f7bc2d0313e5c08a57b61a65b606e2bf17ae41
hobeomcheon <cjs414ghqja@gmail.com>
2021-11-19 10:40:32 +0900

    'v-model'을 활용한 양방향 데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <div :class="nameClass">
-    {{ name }}
-  </div>
-  <!--  'v-bind:' 는 ':' 로 생략가능-->
-  <input :type="type" :value="name">
-  <!--  'v-on:' 는 '@' 로 생략가능-->
+  <input
+      type="text"
+      v-model="name"
+  >
   <button
       class="btn btn-primary"
-      @click="updateName"
+      @click="onSubmit"
   >
     Click
   </button>
@@ -16,32 +14,18 @@
 <script>
 // 템플릿에서도 동시에 값이 변경되기위해 추가해야함
 import { ref } from 'vue';       // ref : 기본자료형
-import { reactive } from 'vue';  // reactive : Object, Array
 
 export default {
   setup() {
     const name = ref('hobeom coder');
-    const type = ref('number');
-    const nameClass = ref('');
-    const name2 = reactive({
-      id : 1
-    });
 
-    const updateName = () => {
-      //
-      name.value = 'change coder';   // ref를 사용할 때는 name.value 형태로 사용
-      type.value = 'text';
-      nameClass.value = 'name';
-      name2.id = 2;                  // 안에 들어있는 값을 바로 접근 가능
-      console.log(name);
-    };
+    const onSubmit = () => {
+      console.log(name.value);
+    }
 
     return {
       name,
-      type,
-      nameClass,
-      name2,
-      updateName,
+      onSubmit,
     };
   }
 }
