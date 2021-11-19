653fcc34d565537498eb07cfbb89eb6bcfb3a897
hobeomcheon <cjs414ghqja@gmail.com>
2021-11-19 19:10:50 +0900

    'v-show' 와 'v-if' 활용

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
+<!--  <div v-if="toggle">true</div>
+  <div v-else>false</div>
+  <button @click="onToggle">Toggle</button>-->
+
   <div class="container">
     <h2>To-Do List</h2>
-    <form
-        @submit.prevent="onSubmit"
-        class="d-flex"
-    >
-      <div class="flex-grow-1">
-        <input
-            class="form-control"
-            type="text"
-            v-model="todo"
-            placeholder="Type new to-do"
-        >
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1">
+          <input
+              class="form-control"
+              type="text"
+              v-model="todo"
+              placeholder="Type new to-do"
+          >
+        </div>
+        <div class="mx-2">
+          <button
+              class="btn btn-primary"
+              type="submit"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div class="mx-2">
-        <button
-            class="btn btn-primary"
-            type="submit"
-        >
-          Add
-        </button>
+      <div v-show="hasError" style="color : red">
+        This field cannot be empty
       </div>
     </form>
 
@@ -41,25 +47,38 @@
 import { ref } from 'vue';       // ref : 기본자료형
 
 export default {
-  data() {
+  setup() {
+    const toggle = ref(false);
     const todo = ref('');
     const todos = ref([
       {id: 1, subject: '휴대폰 사기'},
       {id: 2, subject: '장보기'}
     ]);
+    const hasError = ref(false);
 
     const onSubmit = () => {
       //e.preventDefault();  // submit 새로고침 방지
-      todos.value.push({
-        id : Date.now(),
-        subject : todo.value
-      })
+      if (todo.value === '') {
+        hasError.value = true;
+      } else {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value
+        })
+        hasError.value = false;
+      }
+    }
+    const onToggle = () => {
+      toggle.value = !toggle.value
     }
 
     return {
       todo,
+      toggle,
       todos,
+      hasError,
       onSubmit,
+      onToggle,
     };
   }
 }
